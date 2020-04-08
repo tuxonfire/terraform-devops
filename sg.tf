9cb00558307c6087f315ca332c9bde65e14cb046
Your Name <you@example.com>
2020-04-08 23:04:45 +0000

    update sg file

---
+++ sg.tf
@@ -0,0 +1,38 @@
+resource "aws_security_group" "allow_tls" {
+  name        = "bootcamp_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "aws_default_vpc.id"
+
+  ingress {
+    description = "ssh from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "http from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
